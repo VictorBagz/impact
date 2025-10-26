4e8428291b93e316c1939c5343385f2afa97d152
VictorBagz <victorbaguma@example.com>
2025-10-26 08:59:32 +0300

    modified

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()], // Add this - it's required for React
+  plugins: [react()],
   base: '/newImpact/',
   resolve: {
     alias: {
