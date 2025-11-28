e4e6dc8837f2f5c7d05612d6dae87b9315604bb1
VictorBagz <victorbaguma@example.com>
2025-11-28 15:11:51 +0300

    modified

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VERCEL ? '/' : '/newimpact/',
+  base: process.env.VERCEL ? '/' : '/impact/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
